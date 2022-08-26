e429b7a53d38d893cea70a32aeb8b0fc47004a7f
SHsuperCM <shsupercm@gmail.com>
2022-08-26 10:12:52 +0300

    Added shortcut to add fungible to dev environment

--- src/main/java/io/shcm/shsupercm/fabric/fletchingtable/FletchingTableExtension.java
+++ src/main/java/io/shcm/shsupercm/fabric/fletchingtable/FletchingTableExtension.java
@@ -1,9 +1,17 @@
 package io.shcm.shsupercm.fabric.fletchingtable;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.gradle.api.Project;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.compile.JavaCompile;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -63,5 +71,43 @@ public abstract class FletchingTableExtension {
         compilerArgs.add("-Afletchingtable.mixins.default=" + getDefaultMixinEnvironment().get());
         compilerArgs.add("-Afletchingtable.mixins.prefix.client=" + getAutoMixinEnvironmentClientPrefix().get());
         compilerArgs.add("-Afletchingtable.mixins.prefix.server=" + getAutoMixinEnvironmentServerPrefix().get());
+    }
+
+    /**
+     * Downloads and adds Fungible(by magistermaks) to the running development environment.
+     * @see <a href="https://github.com/magistermaks/mod-fungible">github.com/magistermaks/mod-fungible</a>
+     * @param tag release tag from <a href="https://github.com/magistermaks/mod-fungible/tags">fungible's github releases page</a>.
+     */
+    public void fungible(String tag) {
+        System.out.println("Fletching Table: Applying fungible by magistermaks");
+        try {
+            File jarsDir = new File(project.getProjectDir(), ".gradle/fletchingtable/jars");
+            jarsDir.mkdirs();
+
+            File fungibleJar = new File(jarsDir, "fungible-" + tag + ".jar");
+
+            if (!fungibleJar.exists()) {
+                System.out.println("Fletching Table: Downloading fungible...");
+                URL downloadUrl;
+                try (InputStreamReader isr = new InputStreamReader(new URL("https://api.github.com/repos/magistermaks/mod-fungible/releases/tags/" + tag).openStream())) {
+                    JsonObject tagInfo = new Gson().fromJson(isr, JsonObject.class);
+                    JsonArray assets = tagInfo.getAsJsonArray("assets");
+                    if (assets == null || assets.size() < 1)
+                        throw new Exception("Could not resolve tag.");
+                    JsonObject asset = assets.get(0).getAsJsonObject();
+                    JsonElement jsonDownloadUrl = asset.get("browser_download_url");
+                    if (jsonDownloadUrl == null)
+                        throw new Exception("Could not resolve tag.");
+                    downloadUrl = new URL(jsonDownloadUrl.getAsString());
+                }
+
+                FileUtils.copyURLToFile(downloadUrl, fungibleJar);
+            }
+
+            project.getDependencies().add("modRuntimeOnly", project.files(new File(project.getProjectDir(), ".gradle/fletchingtable/jars/fungible-" + tag + ".jar")));
+        } catch (Exception e) {
+            System.out.println("Could not apply fungible to the running environment with tag \"" + tag + "\"");
+            e.printStackTrace();
+        }
     }
 }
