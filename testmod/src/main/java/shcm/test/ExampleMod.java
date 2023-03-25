69a4a6e84b79ccdc174eab7e40517cae99cf443e
SHsuperCM <shsupercm@gmail.com>
2023-03-25 11:46:07 +0300

    Added test mod subproject

---
+++ testmod/src/main/java/shcm/test/ExampleMod.java
@@ -0,0 +1,11 @@
+package shcm.test;
+
+import io.shcm.shsupercm.fabric.fletchingtable.api.Entrypoint;
+
+public class ExampleMod {
+
+	@Entrypoint(Entrypoint.CLIENT)
+	public static void onInitialize() {
+
+	}
+}
